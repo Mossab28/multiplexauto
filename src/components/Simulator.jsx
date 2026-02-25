90c33833bfc9c17166ffe6d9036de1f701a7e1f8
Mirande-Ney Moss'Ab <mossab@MacBook-Air-de-Mirande-Ney.local>
2026-02-25 15:32:42 +0100

    fix: augmenter padding top du simulateur pour éviter collision avec navbar

--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -75,7 +75,7 @@ const Simulator = () => {
   }
 
   return (
-    <div className="min-h-screen bg-matte-black pt-24 pb-12">
+    <div className="min-h-screen bg-matte-black pt-32 sm:pt-36 pb-12">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto">
           {/* Header */}
